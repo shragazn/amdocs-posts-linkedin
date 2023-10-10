8bf53347c9161c2562b606723d7a8f8603c240b3
shragazn <shragazn@gmail.com>
2023-10-11 00:00:18 +0300

    removed clg

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const main = async () => {
   const user = await getInfluencer(
     "https://www.linkedin.com/in/ido-zrihen-b42a431b9"
   );
-  console.log("🚀 ~ file: index.ts:5 ~ main ~ user:", user);
+
 };
 
 main();
